bbc2707ccd54fe2d570c0540770b576e0754f149
Luke Leiter <ldlstarwars@gmail.com>
2018-01-24 13:14:27 -0500

    Creating the Evironment
    Used the interior prefab
    Created Lighting
    Added a hoop
    Created scripts to control the controller behavior
    can pick up object

---
+++ Basket Ball VR/Assets/Scripts/ContollerBehavior.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContollerBehavior : MonoBehaviour
+{
+
+    private SteamVR_TrackedObject trackedObj;
+    private GameObject collidingObject;
+    private GameObject objectInHand;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    private void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void Update()
+    {
+        if (Controller.GetHairTriggerDown())
+        {
+            if (collidingObject)
+            {
+                GrabObject();
+            }
+        }
+
+        if (Controller.GetHairTriggerUp())
+        {
+            if (objectInHand)
+            {
+                ReleaseObject();
+            }
+        }
+    }
+
+    //Set a touched rigidboy to a 'CollidingObject'
+    private void SetCollidingObject(Collider col)
+    {
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+        collidingObject = col.gameObject;
+    }
+
+    //If there's an object being touched by a trigger collider, set it as the colliding object
+    //if not, do nothing and set the colliding object as null
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+        collidingObject = null;
+    }
+
+    private void GrabObject()
+    {
+        //Set the object holding to the collided object then set a collided object to null
+        objectInHand = collidingObject;
+        collidingObject = null;
+
+        //create a connection between the controller and the collided object
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    private void ReleaseObject()
+    {
+        
+        if(GetComponent<FixedJoint>())
+        {
+            //find the connection between the object and the controller and break it
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+            //set the object in the same direction the controller is moving
+            objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+        }
+        //allow another object to be picked up
+        objectInHand = null;
+    }
+
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+}
+
