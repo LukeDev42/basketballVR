cdbe71faf7bc74c771705be60099b445887bbc6b
Luke Leiter <ldlstarwars@gmail.com>
2018-01-29 13:14:10 -0500

    Attempting to Make a score text object
    *can't get the score to set to one and only add when the collider is triggered

--- Basket Ball VR/Assets/Scripts/ScoreManager.cs
+++ Basket Ball VR/Assets/Scripts/ScoreManager.cs
@@ -1,16 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public Text scoreText;
+    public BoxCollider ScoreKeeper;
+
+    int newScoreValue = 0;
+    int score;
+
+    private void Awake()
+    {
+        scoreText = GetComponent<Text>();
+        score = 0;
+        UpdateScore();
+    }
+
+    void Update()
+    {
+        if (ScoreKeeper)
+        {
+            UpdateScore();
+        }
+        AddScore(newScoreValue);
+        Debug.Log(newScoreValue);
+        scoreText.text = " " + score;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    //Add to the actual score
+    void AddScore (int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+    //Find the new score value
+    int UpdateScore()
+    {
+        if(ScoreKeeper)
+        {
+            newScoreValue = 1 + newScoreValue;
+        }
+        return newScoreValue;
+    }
 }
