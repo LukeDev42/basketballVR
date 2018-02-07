8a4271214de5c8336253b6de8ee13c021f6a15c9
Luke Leiter <ldlstarwars@gmail.com>
2018-02-07 13:12:21 -0500

    Score Counting
    *got the score to count
    *Used two scripts instead of one:
    	*an OnTriggerEnter script
    	*a scoreAdder script
    *used FindObjectOfType to find ScoreAdder script insteaed of GetComponent
    *next step is to make game more realistic shooting

---
+++ Basket Ball VR/Assets/Scripts/ScoreAdder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreAdder : MonoBehaviour {
+
+    static TextMesh scoreText;
+    static int currentScore = 0;
+
+    private void Start()
+    {
+        scoreText = GameObject.FindGameObjectWithTag("ScoreText").GetComponent<TextMesh>();
+        UpdateScore(currentScore);
+    }
+
+    public void UpdateScore(int addedValue)
+    {
+        currentScore += addedValue;
+        scoreText.text = "Score: " + currentScore;
+    }
+}
--- Basket Ball VR/Assets/Scripts/ScoreManager.cs
+++ Basket Ball VR/Assets/Scripts/ScoreManager.cs
@@ -1,44 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
 
-    public TextMesh scoreText;
-    public BoxCollider scoreKeeper;
-    public static int score;
+    private ScoreAdder scoreAdder;
 
-    private void Awake()
+    private void Start()
     {
-        scoreKeeper = GetComponent<BoxCollider>();
-        score = 0;
-        UpdateScore();
+        scoreAdder = GameObject.FindObjectOfType<ScoreAdder>();
     }
-
-    void Update()
-    {
-        Debug.Log(score);
-        
-	}
 
     private void OnTriggerEnter(Collider col)
     {
         switch(col.tag)
         {
             case "Trigger":
-                AddScore(1);
+                scoreAdder.UpdateScore(1);
                 break;
         }
-    }
-
-    void AddScore(int newScoreValue)
-    {
-        score += newScoreValue;
-        UpdateScore();
-    }
-    void UpdateScore()
-    {
-        scoreText.text = "Score: " + score;
     }
 }
