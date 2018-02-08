a525ec2896c20de8886c7780c1ec9f947aafee84
Luke Leiter <ldlstarwars@gmail.com>
2018-02-08 12:01:19 -0500

    Basketball Bounce
    *gave the ball its own material
    *bounces much like a real basketball

--- Basket Ball VR/Assets/Scripts/ScoreManager.cs
+++ Basket Ball VR/Assets/Scripts/ScoreManager.cs
@@ -8,7 +8,7 @@ public class ScoreManager : MonoBehaviour {
 
     private void Start()
     {
-        scoreAdder = GameObject.FindObjectOfType<ScoreAdder>();
+        scoreAdder = FindObjectOfType<ScoreAdder>();
     }
 
     private void OnTriggerEnter(Collider col)
